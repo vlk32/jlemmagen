71cc2845ece6b0c67afd967c391d5a3b8536aa83
Vladimir Kralik <vkralik@gmail.com>
2020-07-16 11:45:35 +0200

    Generationg artifacts for source and javadoc

--- src/main/java/eu/hlavki/text/lemmagen/impl/LemmaExample.java
+++ src/main/java/eu/hlavki/text/lemmagen/impl/LemmaExample.java
@@ -111,7 +111,7 @@ public class LemmaExample implements Comparable<LemmaExample> {
     /**
      * Lemma to be produced by pre-lemmatizing with Front-Lemmatizer (Warning it is reversed)
      *
-     * @return
+     * @return produced lemma
      */
     public String getLemmaFront() {
         if (lemmaFrontCache == null) {
@@ -125,7 +125,7 @@ public class LemmaExample implements Comparable<LemmaExample> {
      * word to be lemmatized by standard Rear-Lemmatizer (it's beggining has been already modified by
      * Front-Lemmatizer)
      *
-     * @return
+     * @return lemmatized word
      */
     public String getWordRear() {
         if (wordRearCache == null) {
@@ -143,7 +143,7 @@ public class LemmaExample implements Comparable<LemmaExample> {
     /**
      * lemma to be produced by standard Rear-Lemmatizer from WordRear
      *
-     * @return
+     * @return lemma
      */
     public String getLemmaRear() {
         return lemma;
